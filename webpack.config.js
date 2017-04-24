12424224fb301e2f1792afc6368de08ba19005ab
Patricio de la Guardia <pdelaguardia87@gmail.com>
2017-04-24 15:20:06 -0400

    Install lodash, add task manager tasks for webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './dist/index.js',
+    entry: './appSrc/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
