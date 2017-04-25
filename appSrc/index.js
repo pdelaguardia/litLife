9fa58551f1d91b84ec890b0e8ca4f74a7b7125b5
Patricio de la Guardia <pdelaguardia87@gmail.com>
2017-04-25 07:27:26 -0400

    Add mock express server

--- appSrc/index.js
+++ appSrc/index.js
@@ -1,1 +1,10 @@
 import _ from 'lodash';
+import express from 'express';
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+})
+
+app.listen(3000, function() { console.log("Example app running on port 3000") });
